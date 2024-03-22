1604c1d770249cbb6e74c8b950128fa23b382f70
JackReaper <domcapour@seznam.cz>
2024-03-22 17:29:31 +0100

    Added queue fot GameObjects to be added

--- src/main/java/Game/Handler.java
+++ src/main/java/Game/Handler.java
@@ -6,13 +6,16 @@ import java.util.LinkedList;
 
 public class Handler {
     //Linked list for all game objects
-    public LinkedList<GameObject> objects = new LinkedList<GameObject>();
+    public LinkedList<GameObject> objects = new LinkedList<>();
+    //Linked list of game object that will be added the next tick
+    public LinkedList<GameObject> toAdd = new LinkedList<>();
 
     //Ticks all game objects
     public void tick(){
         for (GameObject go : objects){
             go.tick();
         }
+        addToList();
     }
     //Renders all game objects
     public void render(Graphics g){
@@ -20,9 +23,14 @@ public class Handler {
             go.render(g);
         }
     }
+    //Method to add game object to the list
+    public void addToList(){
+        objects.addAll(toAdd);
+        toAdd.clear();
+    }
     //Adds game object
     public void addGameObject(GameObject gameObject){
-        this.objects.add(gameObject);
+        this.toAdd.add(gameObject);
     }
     //Removes game object
     public void removeGameObject(GameObject gameObject){
