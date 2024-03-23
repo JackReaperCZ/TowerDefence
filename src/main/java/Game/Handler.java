42b958fec9f33c4538759983a01925cc221b2f1b
JackReaper <domcapour@seznam.cz>
2024-03-23 20:54:56 +0100

    Added map render system

--- src/main/java/Game/Handler.java
+++ src/main/java/Game/Handler.java
@@ -2,6 +2,7 @@ package Game;
 
 import java.awt.Graphics;
 import java.util.LinkedList;
+import Game.Map.Map;
 
 
 public class Handler {
@@ -9,16 +10,24 @@ public class Handler {
     public LinkedList<GameObject> objects = new LinkedList<>();
     //Linked list of game object that will be added the next tick
     public LinkedList<GameObject> toAdd = new LinkedList<>();
+    //Linked list of game object that will be removed the next tick
+    public LinkedList<GameObject> toRemove = new LinkedList<>();
+    //Actual map to render
+    public Map map = new Map();
+
+    private int idFlag = 0;
 
     //Ticks all game objects
     public void tick(){
         for (GameObject go : objects){
             go.tick();
         }
+        removeFromList();
         addToList();
     }
     //Renders all game objects
     public void render(Graphics g){
+        map.render(g);
         for (GameObject go : objects){
             go.render(g);
         }
@@ -28,12 +37,19 @@ public class Handler {
         objects.addAll(toAdd);
         toAdd.clear();
     }
+    //Method to remove game object from the list
+    public void removeFromList(){
+        objects.removeAll(toRemove);
+        toRemove.clear();
+    }
     //Adds game object
     public void addGameObject(GameObject gameObject){
+        gameObject.setIdentification(this.idFlag);
+        idFlag++;
         this.toAdd.add(gameObject);
     }
     //Removes game object
     public void removeGameObject(GameObject gameObject){
-        this.objects.remove(gameObject);
+        this.toRemove.add(gameObject);
     }
 }
