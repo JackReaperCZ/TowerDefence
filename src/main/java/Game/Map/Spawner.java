14657994800af27803ace38216f3b50ff632a8da
JackReaper <domcapour@seznam.cz>
2024-03-27 22:17:16 +0100

    Added Spawner.java class

---
+++ src/main/java/Game/Map/Spawner.java
@@ -0,0 +1,24 @@
+package Game.Map;
+
+import Game.Handler;
+import Game.Monsters.Monster;
+
+public class Spawner {
+    private int counter = 0;
+    private Path path;
+    private Handler handler;
+
+    public Spawner(Path path, Handler handler) {
+        this.handler = handler;
+        this.path = path;
+    }
+    public void tick(){
+        if (counter == 240){
+            Monster m = new Monster(path.getFlag(0).getX(),path.getFlag(0).getY(),2,handler);
+            m.goTo(path.getFlag(1).getX(),path.getFlag(1).getY());
+            handler.addGameObject(m);
+            counter = 0;
+        }
+        counter++;
+    }
+}
