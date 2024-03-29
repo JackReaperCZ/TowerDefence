0f64e7e195b579ec287844af21ad040346da7926
JackReaper <domcapour@seznam.cz>
2024-03-29 14:14:08 +0100

    Added cannonball projectile

---
+++ src/main/java/Game/Towers/Projectile/Cannonball.java
@@ -0,0 +1,16 @@
+package Game.Towers.Projectile;
+
+import Game.Handler;
+
+import javax.swing.*;
+
+public class Cannonball extends Projectile{
+    public Cannonball(int x, int y, int x2, int y2, Handler handler) {
+        super(x, y, handler);
+        this.speed = 25;
+        velCalculate(x2, y2, speed);
+        ImageIcon icon = new ImageIcon("src/main/data/projectile/cannonball/cannonball.png");
+        this.image = icon.getImage();
+    }
+
+}
