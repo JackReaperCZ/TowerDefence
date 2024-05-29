3aacb55f09a27ca81879d44ec2810220a37a1053
JackReaperCZ <domcapour@seznam.cz>
2024-05-29 09:54:36 +0200

    Delete src directory

--- src/java/Game/Monsters/Type.java
+++
@@ -1,9 +0,0 @@
-package Game.Monsters;
-/**
- * Enumerates the special types of monsters in the game.
- */
-public enum Type {
-    NORMAL,
-    ARMORED,
-    MAGICRESISTANT,
-}
--- src/java/Game/Towers/Projectile/DamageType.java
+++
@@ -1,8 +0,0 @@
-package Game.Towers.Projectile;
-/**
- * Enum representing the types of damage that can be inflicted by projectiles in the game.
- */
-public enum DamageType {
-    NORMAL,
-    MAGICAL,
-}
